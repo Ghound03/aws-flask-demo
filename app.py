a77f36be75d2e415a75c1140a877c9ddef87e4cc
Graham King-Abor <grahamkingaborlm10@gmail.com>
2026-09-21 10:50:08 +0100

    Initial Flask application setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello from my first AWS Flask project!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
