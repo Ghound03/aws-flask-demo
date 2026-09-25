53cbee669cdfbb62a34c09e543e049fc7ce23314
Graham King-Abor <grahamkingaborlm10@gmail.com>
2026-09-25 15:56:25 +0100

    Add homepage and health check endpoint

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello from my first AWS Flask project!"
+    return render_template("index.html")
+
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}, 200
 
 
 if __name__ == "__main__":
